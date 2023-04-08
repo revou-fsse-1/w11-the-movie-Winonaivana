ca45f470868dfb646d80dfa43a8a9c2f733b90cb
winona ivana <winonaivana@gmail.com>
2023-04-08 11:06:53 +0100

    add search function to watchlist

--- src/watchlist.js
+++ src/watchlist.js
@@ -3,6 +3,9 @@ const API = `http://localhost:3000/watchlist`;
 const watchlist = document.getElementById("watchlist-grid");
 const row = document.getElementById("row");
 let output = "";
+
+watchlist.classList.remove("hidden");
+
 const showMovies = (movies) => {
   movies.forEach((movie) => {
     output += `
@@ -67,3 +70,94 @@ if (loggedData === false) {
 }
 
 const searchWatchlist = document.getElementById("watchlistSearch");
+const grid5 = document.getElementById("search-grid");
+const texts = document.getElementById("text");
+let output5 = "";
+
+function showResult(event) {
+  event.preventDefault();
+
+  const value = searchWatchlist.value.toLowerCase();
+  fetch(API)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      if (value.length < 2) {
+        grid5.innerHTML = "";
+        grid5.classList.add("hidden");
+        texts.classList.add("hidden");
+      } else {
+        grid5.innerHTML = "";
+        grid5.classList.remove("hidden");
+        texts.classList.remove("hidden");
+        data.forEach((movie) => {
+          const title = movie.title.toLowerCase();
+
+          if (title.includes(value)) {
+            output5 = `
+            
+            <div class="w-[130px] h-[200px] lg:min-w-[155px] lg:min-h-[227px] md:min-w-[155px] md:min-h-[227px]   rounded-[20px] relative overflow-hidden">
+        <img class="w-[100%] object-cover h-[200px] lg:h-[227px] md:h-[227px] rounded-[20px] " src="${movie.image}">
+        <p
+      class="w-[100%] h-[100%] absolute  text-white/0 hover:text-white text-md font-bold top-0 flex justify-center items-center hover:bg-black/50   "
+      id =${movie.id}
+      "
+      >
+      ${movie.rating}
+    </p>
+        </div>
+
+            `;
+
+            grid5.innerHTML += output5;
+          }
+        });
+      }
+    });
+}
+
+const searchWatchlist2 = document.getElementById("watchlistSearch2");
+
+function showResult2(event) {
+  event.preventDefault();
+
+  const value = searchWatchlist2.value.toLowerCase();
+  fetch(API)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      if (value.length < 2) {
+        grid5.innerHTML = "";
+        grid5.classList.add("hidden");
+        texts.classList.add("hidden");
+      } else {
+        grid5.innerHTML = "";
+        grid5.classList.remove("hidden");
+        texts.classList.remove("hidden");
+        data.forEach((movie) => {
+          const title = movie.title.toLowerCase();
+
+          if (title.includes(value)) {
+            output5 = `
+            
+            <div class="w-[130px] h-[200px] lg:min-w-[155px] lg:min-h-[227px] md:min-w-[155px] md:min-h-[227px]   rounded-[20px] relative overflow-hidden">
+        <img class="w-[100%] object-cover h-[200px] lg:h-[227px] md:h-[227px] rounded-[20px] " src="${movie.image}">
+        <p
+      class="w-[100%] h-[100%] absolute  text-white/0 hover:text-white text-md font-bold top-0 flex justify-center items-center hover:bg-black/50   "
+      id =${movie.id}
+      "
+      >
+      ${movie.rating}
+    </p>
+        </div>
+
+            `;
+
+            grid5.innerHTML += output5;
+          }
+        });
+      }
+    });
+}
