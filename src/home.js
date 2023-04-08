9d48ede55936bcfc057005dbf8b0dfc35195b43a
winona ivana <winonaivana@gmail.com>
2023-04-08 10:01:31 +0100

    fix search function

--- src/home.js
+++ src/home.js
@@ -159,7 +159,7 @@ let output5 = "";
 
 function searchMoviess(e) {
   e.preventDefault();
-  console.log("hello");
+
   const value = searchMovie.value.toLowerCase();
   fetch(API + "movies")
     .then((response) => {
